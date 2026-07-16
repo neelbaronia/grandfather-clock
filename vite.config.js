7029f951f39102c09a9f23de43ba946bb8cac744
Neel Baronia <nbaronia@gmail.com>
2026-07-15 21:11:37 -0700

    Add visualization direction mockups

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: new URL('./index.html', import.meta.url).pathname,
+        designDirections: new URL('./design-directions.html', import.meta.url).pathname,
+      },
+    },
+  },
 })
